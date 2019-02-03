4a92f66b9cead28fa43847631f972467da9e5edc
mutsuoKudo <mukudo@fa2.so-net.ne.jp>
2019-02-03 18:56:20 +0900

    全PCリスト表示

---
+++ tpl/footer.php
@@ -0,0 +1,10 @@
+<?php ?>
+<footer id="footer">
+    <a href="index.php.html">ホーム</a>　|　<a href="goaisatu.html">PCリスト</a>　|　<a href="information.html">業務内容</a>　|　<a href="profile.html">会社概要</a>　|　<a href="faq.html">よくあるご質問</a>　|　<a href="contact.html">お問い合わせ</a>　|　<a href="link.html">リンク</a>　|　<a href="sitemap.html">サイトマップ</a>
+    <br>
+    <br>
+    <address>
+        Copyright (C) 2019 hokushin_util All Rights Reserved.　design by <a href="http://tempnate.com/" target="_blank">tempnate</a>
+    </address>
+</footer>
+
